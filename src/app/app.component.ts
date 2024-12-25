c717f9fed95c1433ba258117b4ae5c150dbf461a
Samiha Hatem <Samiha.Hatem@citystars.com.eg>
2024-12-25 15:20:36 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'constraTech';
+
+  @ViewChild('videoPlayer') videoPlayer: any;
+  autoPlayFlag = true;
+
+  ngAfterViewInit() {
+    // Check if video is muted and play it
+    const videoElement = this.videoPlayer.nativeElement;
+    videoElement.muted = true; // Ensure video is muted
+    videoElement.play();       // Trigger autoplay
+  }
+
 }
